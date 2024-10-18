4b91b2dd71e4d73411772d26c4f696f95d39bd89
Nico Dann <nicodann@gmail.com>
2024-10-18 10:20:37 -0400

    Clean up logs.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,10 +3,14 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1];
 
+  console.log("token:",token)
+
   if (!token) return res.status(401).json({ error: 'Access denied' });
 
   try {
+    console.log("trying")
     const decoded = jwt.verify(token, 'secret');
+    console.log("decoded:",decoded)
     req.userId = decoded.userId;
     next();
   } catch (error) {
